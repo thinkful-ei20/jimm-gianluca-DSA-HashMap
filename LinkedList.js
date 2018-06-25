03635446df9979cfda38a10a9dd8af9749a815bd
patersog <gianluca.paterson@gmail.com>
2018-06-25 16:22:28 -0400

    Update resize for sep-chain

--- LinkedList.js
+++ LinkedList.js
@@ -1,143 +1,144 @@
+
 class _Node {
-  constructor(value, next){
-    this.value = value;
-    this.next = next;
-  }
+	constructor(value, next){
+		this.value = value;
+		this.next = next;
+	}
 }
 
 class LinkedList {
-  constructor(){
-    this.head = null;
-  }
+	constructor(){
+		this.head = null;
+	}
 
-  insertFirst(item){
-    this.head = new _Node(item, this.head);
-  }
+	insertFirst(item){
+		this.head = new _Node(item, this.head);
+	}
 
-  insertLast(item){
-    if (this.head === null) {
-      this.insertFirst(item);
-    } else {
-      let tempNode = this.head;
-      while(tempNode.next!==null){
-        tempNode = tempNode.next;
-      }
-      tempNode.next = new _Node(item, null);
-    }
-  }
+	insertLast(item){
+		if (this.head === null) {
+			this.insertFirst(item);
+		} else {
+			let tempNode = this.head;
+			while(tempNode.next!==null){
+				tempNode = tempNode.next;
+			}
+			tempNode.next = new _Node(item, null);
+		}
+	}
 
-  insertBefore(before, item){
-    if(!this.head){
-      console.log('List is empty. Nothing to insert before.');
-      return;
-    }
-    // if before is the first value
-    if(this.head.value === before){
-      this.insertFirst(item);
-      return;
-    }
-    let cNode = this.head; 
-    let pNode = this.head;
-    while((cNode !== null) && (cNode.value !== before)){
-      pNode = cNode;
-      cNode = cNode.next;
-    }
-    if (cNode === null) {
-      console.log('Before ttem not found. Could not insert.');
-      return;
-    } else {
-      pNode.next = new _Node(item, cNode);
-    }
-  }
+	insertBefore(before, item){
+		if(!this.head){
+			console.log('List is empty. Nothing to insert before.');
+			return;
+		}
+		// if before is the first value
+		if(this.head.value === before){
+			this.insertFirst(item);
+			return;
+		}
+		let cNode = this.head;
+		let pNode = this.head;
+		while((cNode !== null) && (cNode.value !== before)){
+			pNode = cNode;
+			cNode = cNode.next;
+		}
+		if (cNode === null) {
+			console.log('Before ttem not found. Could not insert.');
+			return;
+		}
+		pNode.next = new _Node(item, cNode);
 
-  insertAfter(after, item){
-    if(!this.head){
-      console.log('List is empty. Nothing to insert before.');
-      return;
-    }
-    if(this.head.next === null){
-      this.insertLast(item);
-      return;
-    }
-    let aNode = this.head; 
-    let pNode = this.head;
-    while((aNode !== null) && (aNode.value !== after)){
-      pNode = aNode;
-      aNode = aNode.next;
-    }
-    pNode = pNode.next;
-    aNode = aNode.next;
-    if (aNode === null) {
-      console.log('After item not found. Could not insert.');
-      return;
-    } else {
-      pNode.next = new _Node(item, aNode);
-    }
-  }
+	}
 
-  insertAt(pos, item){
-    if(pos === 0){
-      this.insertFirst(item);
-      return;
-    }
-    let cNode = this.head; 
-    for(let i = 0; i < pos; i++){
-      cNode = cNode.next;
-      if(cNode.next === null){
-        console.log('position is outside of list');
-        return;
-      }
-    }
-    if (cNode === null) {
-      this.insertLast(item);
-      return;
-    } else {
-      this.insertBefore(cNode, item);
-    }
-  }
+	insertAfter(after, item){
+		if(!this.head){
+			console.log('List is empty. Nothing to insert before.');
+			return;
+		}
+		if(this.head.next === null){
+			this.insertLast(item);
+			return;
+		}
+		let aNode = this.head;
+		let pNode = this.head;
+		while((aNode !== null) && (aNode.value !== after)){
+			pNode = aNode;
+			aNode = aNode.next;
+		}
+		pNode = pNode.next;
+		aNode = aNode.next;
+		if (aNode === null) {
+			console.log('After item not found. Could not insert.');
+			return;
+		}
+		pNode.next = new _Node(item, aNode);
 
-  // model like `insert` later on
-  remove(item){
-    // If no list, no delete
-    if(!this.head){
-      return null;
-    }
-    // If removing first item, change the head
-    if(this.head.value.key===item){
-      this.head = this.head.next;
-      return;
-    }
-    // If not at the head, traverse the list until you find the item,
-    // or until you reach the end of the list
-    let cNode = this.head; 
-    let pNode = this.head;
-    while((cNode !== null) && (cNode.value.key !== item)){
-      pNode = cNode;
-      cNode = cNode.next;
-    }
-    if (cNode === null) {
-      console.log('Item not found. Could not delete.');
-      return;
-    } else {
-      pNode.next = cNode.next;
-    }
-  }
+	}
 
-  find(item){
-    let cNode = this.head;
-    if(!this.head){
-      return null;
-    }
-    while((cNode.value.key !== item)){
-      if(cNode.next === null){
-        console.log('Item not found.');
-        return false;
-      } else {
-        cNode =cNode.next;
-      }  
-    }
-    return cNode;
-  }
+	insertAt(pos, item){
+		if(pos === 0){
+			this.insertFirst(item);
+			return;
+		}
+		let cNode = this.head;
+		for(let i = 0; i < pos; i++){
+			cNode = cNode.next;
+			if(cNode.next === null){
+				console.log('position is outside of list');
+				return;
+			}
+		}
+		if (cNode === null) {
+			this.insertLast(item);
+			return;
+		}
+		this.insertBefore(cNode, item);
+
+	}
+
+	// model like `insert` later on
+	remove(item){
+		// If no list, no delete
+		if(!this.head){
+			return null;
+		}
+		// If removing first item, change the head
+		if(this.head.value.key===item){
+			this.head = this.head.next;
+			return;
+		}
+		// If not at the head, traverse the list until you find the item,
+		// or until you reach the end of the list
+		let cNode = this.head;
+		let pNode = this.head;
+		while((cNode !== null) && (cNode.value.key !== item)){
+			pNode = cNode;
+			cNode = cNode.next;
+		}
+		if (cNode === null) {
+			console.log('Item not found. Could not delete.');
+			return;
+		}
+		pNode.next = cNode.next;
+
+	}
+
+	find(item){
+		let cNode = this.head;
+		if(!this.head){
+			return null;
+		}
+		while((cNode.value.key !== item)){
+			if(cNode.next === null){
+				console.log('Item not found.');
+				return false;
+			}
+			cNode =cNode.next;
+
+		}
+		return cNode;
+	}
 }
 
 module.exports = LinkedList;
--- sep-chain.js
+++ sep-chain.js
@@ -2,125 +2,130 @@ const LinkedList = require('./LinkedList');
 
 class HashMap {
 
-  constructor(initialCapacity=8) {
-    this.length = 0;
-    this._slots = [];
-    this._capacity = initialCapacity;
-    this._deleted = 0;
-  }
+	constructor(initialCapacity=8) {
+		this.length = 0;
+		this._slots = [];
+		this._capacity = initialCapacity;
+		this._deleted = 0;
+	}
 
-  static _hashString(string) { //hash the key to find an appropriate index
-    let hash = 5381;
-    for (let i=0 ; i<string.length; i++) {
-      hash = (hash << 5) + hash + string.charCodeAt(i);
-      hash = hash & hash; // make sure it's a 32-bit integer using the & operator on itself.
-    }
-    return hash >>> 0; //return a positive index
-  }
+	static _hashString(string) { //hash the key to find an appropriate index
+		let hash = 5381;
+		for (let i=0 ; i<string.length; i++) {
+			hash = (hash << 5) + hash + string.charCodeAt(i);
+			hash = hash & hash; // make sure it's a 32-bit integer using the & operator on itself.
+		}
+		return hash >>> 0; //return a positive index
+	}
 
-  get(key) {
-    const index = this._findSlot(key);
-    if(this._slots[index] === undefined) {
-      throw new Error('key error');
-    }
-    return this._slots[index].value;
-  }
-  
-  set(key, value) {
-    const loadRatio = (this.length + this._deleted + 1) / this._capacity;
-    if (loadRatio > HashMap.MAX_LOAD_RATIO) {
-      this._resize(this._capacity * HashMap.SIZE_RATIO);
-    }
+	get(key) {
+		const index = this._findSlot(key);
+		if(this._slots[index] === undefined) {
+			throw new Error('key error');
+		}
+		return this._slots[index].find(key).value;
+	}
 
-    const index = this._findSlot(key);
-    if(this._slots[index] === undefined){
-      this._slots[index] = new LinkedList();
-      this._slots[index].insertFirst({key, value, deleted: false});
-      this.length++;      
-    } else {
-      if(this._slots[index].find(key)){
-        this._slots[index].remove(key);
-      } 
-      this._slots[index].insertLast({key, value, deleted: false});
-    }
-    
-  }
+	set(key, value) {
+		const loadRatio = (this.length + this._deleted + 1) / this._capacity;
+		if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+			this._resize(this._capacity * HashMap.SIZE_RATIO);
+		}
 
-  remove(key) {
-    const index = this._findSlot(key);
-    const slot = this._slots[index];
-    if(slot === undefined) {
-      throw new Error('Key error');
-    }
+		const index = this._findSlot(key);
+		if(this._slots[index] === undefined){
+			this._slots[index] = new LinkedList();
+			this._slots[index].insertFirst({key, value, deleted: false});
+			this.length++;
+		} else {
+			if(this._slots[index].find(key)){
+				this._slots[index].remove(key);
+			}
+			this._slots[index].insertLast({key, value, deleted: false});
+		}
 
-    slot.deleted = true;
-    this.length--;
-    this._deleted++;
-  }
+	}
 
-  _findSlot(key) {
-    const hash = HashMap._hashString(key);
-    const start = hash % this._capacity;
-    return start;
+	remove(key) {
+		const index = this._findSlot(key);
+		const slot = this._slots[index];
+		if(slot === undefined) {
+			throw new Error('Key error');
+		}
 
-    // for(let i = start; i < start + this._capacity; i++) {
-    //   const index = i % this._capacity;
-    //   const slot = this._slots[index];
-    //   if(slot === undefined) {
-    //     return index;
-    //   }
-    // }
-  }
+		slot.remove(key)
 
-  _resize(size) {
-    const oldSlots = this._slots;
-    this._capacity = size;
-    this.length = 0;
-    this._slots = [];
-    this._deleted = 0;
+		slot.deleted = true;
+		this.length--;
+		this._deleted++;
+	}
 
-    for(const slot of oldSlots) {
-      if(slot !== undefined && !slot.deleted) {
-        this.set(slot);
-      }
-    }
-  }
+	_findSlot(key) {
+		const hash = HashMap._hashString(key);
+		const start = hash % this._capacity;
+		return start;
+
+		// for(let i = start; i < start + this._capacity; i++) {
+		//   const index = i % this._capacity;
+		//   const slot = this._slots[index];
+		//   if(slot === undefined) {
+		//     return index;
+		//   }
+		// }
+	}
+
+	_resize(size) {
+		const oldSlots = this._slots;
+		this._capacity = size;
+		this.length = 0;
+		this._slots = [];
+		this._deleted = 0;
+
+		for(const slot of oldSlots) {
+			if(slot !== undefined) {
+				let current = slot.head;
+				while(current !== null && (current.value.deleted === false)) {
+					this.set(current.value.key, current.value.value);
+					current = current.next;
+				}
+			}
+		}
+	}
 }
 
-HashMap.MAX_LOAD_RATIO = 0.9;
+HashMap.MAX_LOAD_RATIO = 0.6;
 HashMap.SIZE_RATIO = 3;
 
 function main() {
-  const lor = new HashMap();
-  lor.set('Hobbit','Bilbo');
-  //console.log(lor.get('Hobbit'));
+	const lor = new HashMap();
+	lor.set('Hobbit','Bilbo');
+	//console.log(lor.get('Hobbit'));
 
-  lor.set('Hobbit','Frodo');
-  //console.log(lor.get('Hobbit'));
+	lor.set('Hobbit','Frodo');
+	//console.log(lor.get('Hobbit'));
 
-  // lor.set('Wizard','Gandolf');
-  // lor.set('Human','Aragon');
-  // lor.set('Elf','Legolas');
+	lor.set('Wizard','Gandolf');
+	lor.set('Human','Aragon');
+	lor.set('Elf','Legolas');
 
-  // remains in the hash map until next resize
-  //lor.remove('Hobbit');
-  
-  // lor.set('Maiar','The Necromancer');
-  // lor.set('Maiar','Sauron');
-  // lor.set('RingBearer','Golum');
-  // lor.set('LadyOfLight', 'Galadriel');
-  // lor.set('HalfElven', 'Arwen');
-  // lor.set('Ent', 'Treebeard');
+	console.log(JSON.stringify(lor, null, 2));
 
-  // try{
-  //   console.log(lor.get('Hobbit'));
-  // } catch(err){
-  //   console.log('`lor.get(`Hobbit`)` threw an error, must not exist');
-  // }
-  
-  console.log(lor._slots[5].head);
+	// remains in the hash map until next resize
+	// lor.remove('Hobbit');
 
-  //console.log(JSON.stringify(lor, null, 2));
+	lor.set('Maiar','The Necromancer');
+	lor.set('Maiar','Sauron');
+	lor.set('RingBearer','Golum');
+	lor.set('LadyOfLight', 'Galadriel');
+	// lor.set('HalfElven', 'Arwen');
+	// lor.set('Ent', 'Treebeard');
+
+	// try{
+	//   console.log(lor.get('Hobbit'));
+	// } catch(err){
+	//   console.log('`lor.get(`Hobbit`)` threw an error, must not exist');
+	// }
+	console.log(JSON.stringify(lor, null, 2));
 }
 
 main();
